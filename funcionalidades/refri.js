0e3b95d13d73325ffb3180195e31e76e5e7d5ee0
Alejandro <element3999@gmail.com>
2025-12-05 20:06:56 -0300

    tercera actualización miércoles 05/12

---
+++ funcionalidades/refri.js
@@ -0,0 +1,62 @@
+const btn = document.getElementById('menuBtn');
+const menu = document.getElementById('mobileMenu');
+let inicio = document.getElementById('logo');
+
+inicio.style.cursor = 'pointer';
+inicio.addEventListener('click', () =>{
+    window.location.href = '../index.html';
+});
+
+if(btn){
+    btn.addEventListener('click', ()=>{
+        menu.classList.toggle('hidden');
+    });
+}
+
+if(menu){
+    menu.addEventListener('click', (e)=>{
+        if(e.target === menu) menu.classList.add('hidden');
+    });
+}
+
+//-------------------------------CONTADOR-----------------------------------------------------
+let click = 0;
+let contador = document.getElementById('cantidad_de_productos');
+let numDisp = 2;
+
+let contadorDisp = document.getElementById('disponibles');
+contadorDisp.textContent = numDisp;
+
+let botonResta = document.getElementById('resta');
+botonResta.setAttribute('disabled', null); // anula el botón resta al cargar el documento
+let botonSuma = document.getElementById('suma');
+
+botonResta.addEventListener('click', function(){
+	click = click - 1;
+	contador.textContent = click;
+    numDisp = numDisp + 1;
+    contadorDisp.textContent = numDisp;
+
+    if(numDisp === 2){
+        botonResta.setAttribute('disabled', null);
+        botonSuma.removeAttribute('disabled', null);
+    }
+});
+
+
+botonSuma.addEventListener('click', function(){
+    botonResta.removeAttribute('disabled', null); // activa el botón resta al iniciar la suma de artículos
+	click = click + 1;
+	contador.textContent = click;
+    numDisp = numDisp - 1;
+    contadorDisp.textContent = numDisp;
+    if(numDisp === 0){
+        botonSuma.setAttribute('disabled', null);
+        //botonResta.removeAttribute('disabled', null);
+    }
+});
+//-------------------------------CARRITO---------------------------------------------------
+let agregarCarrito = document.getElementById('agregar-carrito');
+agregarCarrito.addEventListener('click', function(){
+    window.location.href = './carrito.html';
+})
